56e5b70cd249abd62a66054b23a44cdfc122a01c
Hang Wang <hw2437@columbia.edu>
2023-04-30 18:55:14 -0400

    correction

--- main.go
+++ main.go
@@ -18,15 +18,24 @@ func main() {
 		}
 
 		// Update the message type to return back.
-		if body["type"] == "echo" {
-			body["type"] = "echo_ok"
-		} else {
-			body["type"] = "generate_ok"
-			var err error
-			body["id"], err = uuid.NewUUID()
-			if err != nil {
-				panic("cannot generate uuid")
-			}
+		body["type"] = "echo_ok"
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "generate_ok"
+		var err error
+		body["id"], err = uuid.NewUUID()
+		if err != nil {
+			panic("cannot generate uuid")
 		}
 
 		// Echo the original message back with the updated message type.
